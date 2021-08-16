be7ad2a10b0be8c97d0314b335538421599aba1f
victorsantosok <42875522+victorsantosok@users.noreply.github.com>
2021-08-16 20:26:20 -0300

    commit2

---
+++ src/pages/signin/validation.js
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+
+// https://github.com/jquense/yup
+export const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("First Name is required"),
+  lastName: Yup.string().required("Last Name is required"),
+  email: Yup.string()
+    .email("Enter a valid email")
+    .required("Please enter a registered email"),
+  password: Yup.string()
+    .required("Please enter a password")
+});
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Camera from './pages/camera';
 import Main from './pages/main'
 import Signin from './pages/signin/index';
+import RegisterForm from './pages/register';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,17 +13,21 @@ export default function Routes() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen options={{ tabBarIcon: ({color}) => <Feather name="home" size={20} color={color} />}} 
-        name="Login" 
-        component={Signin} />
+        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="home" size={20} color={color} /> }}
+          name="Login"
+          component={Signin} />
 
-        <Tab.Screen options={{ tabBarIcon: ({color}) => <Feather name="aperture" size={20} color={color} />}}  
-        name="Tarefas" 
-        component={Camera} />
+        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="user-plus" size={20} color={color} /> }}
+          name="Registrar"
+          component={RegisterForm} />
 
-        <Tab.Screen options={{ tabBarIcon: ({color}) => <Feather name="at-sign" size={20} color={color} />}} 
-        name="Membros" 
-        component={Main} />
+        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="aperture" size={20} color={color} /> }}
+          name="Tarefas"
+          component={Camera} />
+
+        <Tab.Screen options={{ tabBarIcon: ({ color }) => <Feather name="at-sign" size={20} color={color} /> }}
+          name="Membros"
+          component={Main} />
 
       </Tab.Navigator>
     </NavigationContainer>
